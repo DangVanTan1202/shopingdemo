4e48a84242e940d1b426eeb446a46b58104daa6a
DangVanTan1202 <longnguyen0804vvv@gmail.com>
2024-11-27 09:18:32 +0700

    shop

---
+++ Shopping_Tutorial/Controllers/CheckoutController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping_Tutorial.Models;
+using Shopping_Tutorial.Repository;
+using System.Security.Claims;
+
+namespace Shopping_Tutorial.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly DataContext _dataContext;
+        public CheckoutController(DataContext context)
+        {
+            _dataContext = context;
+        }
+        public async Task<IActionResult> Checkout()
+        {
+            var userEmail = User.FindFirstValue(ClaimTypes.Email);
+            if (userEmail == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                var ordercode = Guid.NewGuid().ToString();
+               var orderItem = new OrderModel();
+                orderItem.OrderCode = ordercode;
+                orderItem.UserName = userEmail;
+                orderItem.Status = 1;
+                orderItem.CreatedDate = DateTime.Now;
+                _dataContext.Add(orderItem);
+                _dataContext.SaveChanges();
+                TempData["success"] = "Đơn Hàng Đã Được Tạo";
+                return RedirectToAction("index","cart");
+
+            }
+                return View();
+            
+            }
+        }
+    }
---
+++ Shopping_Tutorial/Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Shopping_Tutorial.Repository;
+
+namespace Shopping_Tutorial.Controllers
+{
+	public class ProductController : Controller
+	{
+		private readonly DataContext _dataContext;
+		public ProductController(DataContext dataContext)
+		{
+			_dataContext = dataContext;
+		}
+		public async Task<IActionResult> Index()
+		{
+
+			ViewData["searchQuery"] = "searchQuery";
+			return View();
+		}
+
+		public IActionResult Create()
+		{
+			ViewBag.searchQuery = "searchQuery";
+			ViewBag.Categories = new SelectList(_dataContext.Categories, "Id", "Name");
+			ViewBag.Brands = new SelectList(_dataContext.Brands, "Id", "Name");
+			return View();
+		}
+		
+		
+		public async Task<IActionResult> Details(int Id)
+		{
+			if (Id == null) return RedirectToAction("Index");
+			var productsById = _dataContext.Products.Where(c => c.Id == Id).FirstOrDefault();
+			return View(productsById);
+
+		}
+
+		public async Task<IActionResult> Search(string searchQuery)
+		{
+			var products = from p in _dataContext.Products.Include(p => p.Category).Include(p => p.Brand)
+						   select p;
+
+			if (!string.IsNullOrEmpty(searchQuery))
+			{
+				searchQuery = searchQuery.ToLower();
+				products = products.Where(p => EF.Functions.Like(p.Name.ToLower(), $"%{searchQuery}%"));
+				ViewData["searchQuery"] = searchQuery;
+			}
+			else
+			{
+                ViewData["searchQuery"] = string.Empty;
+            }
+
+			return View(await products.OrderByDescending(p => p.Id).ToListAsync());
+		}
+	}
+}
